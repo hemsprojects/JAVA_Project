7d0e8e05d8e0033bea9e3a191f97eaeee8a4ccd4
Hemanath <2326kb17@kgcas.com>
2025-06-19 12:42:35 +0530

    Add files via upload

---
+++ Day07/GradeEval.java
@@ -0,0 +1,16 @@
+package Day07;
+import java.util.Scanner;
+
+public class GradeEval {
+
+	public static void main(String[] args) {
+		try {
+		int i =(Integer)null;
+		System.out.println(i);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
